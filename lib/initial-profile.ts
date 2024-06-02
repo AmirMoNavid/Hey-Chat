c56a923b66c7ee5ab233d43c20a16ca146a847e1
AmirMoNavid <166909046+AmirMoNavid@users.noreply.github.com>
2024-06-02 21:49:52 +0330

    Update initial-profile.ts

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,20 +1,21 @@
-import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+import db from "@/lib/db";
 
-import db from '@/lib/db';
-
-
-/**
- * This initial profile util Returns the current loggedin user, thats it.
- * It handles almost all the edge cases
- * @returns Profile object
- */
-
+import { currentUser } from "@clerk/nextjs/server";
 
 export const initialProfile = async () => {
   const user = await currentUser();
 
-  const username =
-    user?.username.charAt(0).toUpperCase() + user?.username.slice(1);
+  let userName = user?.username;
+
+  const Name = userName?.charAt(0).toLowerCase();
+
+  const secondSlice = userName?.slice(1);
+
+  
+
+  if (!userName) {
+    return null;
+  }
   if (!user) {
     return null;
   }
@@ -30,7 +31,7 @@ export const initialProfile = async () => {
   const newProfile = await db.profile.create({
     data: {
       userId: user.id,
-      name: username,
+      name: `${Name}${secondSlice}`,
       imageUrl: user.imageUrl,
       email: user.emailAddresses[0].emailAddress,
     },
