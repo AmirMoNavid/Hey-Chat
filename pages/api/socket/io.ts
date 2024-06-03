3bb3c433164b09780040eee0e7968865bf9641d4
AmirMoNavid <166909046+AmirMoNavid@users.noreply.github.com>
2024-06-03 12:41:56 +0330

    Update io.ts

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -16,6 +16,7 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
       path: path,
+      addTrailingSlash: false
     });
     res.socket.server.io = io;
   }
